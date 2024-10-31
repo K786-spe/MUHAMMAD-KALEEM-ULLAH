774c069024ef50b6305f394ac2778655a7a502b3
K786-spe <raimuhammadkaleem@gmail.com>
2024-10-31 19:40:07 +0500

    recode

---
+++ LAB 1 +LAB 2 +LAB 3   .cpp
@@ -0,0 +1,376 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "***" << endl;
+//	cout << "* *" << endl;
+//	cout << "***" << endl;
+//	system("pause");
+//	return 0;
+//}
+//
+// 
+// 
+// 
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "  *" << endl;
+//	cout << "*****" << endl;
+//	cout << "  *" << endl;
+//	system("pause");
+//	return 0;
+//}
+//
+// 
+// 
+// 
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << " ***" << endl;
+//	cout << "*****" << endl;
+//	cout << "  *" << endl;
+//	cout << "  *" << endl;
+//	system("pause");
+//	return 0;
+//}
+//
+// 
+// 
+// 
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "2x1=2" << endl;
+//	cout << "2x2=4" << endl;
+//	cout << "2x3=6" << endl;
+//	cout << "2x4=8" << endl;
+//	cout << "2x5=10" << endl;
+//	cout << "2x6=12" << endl;
+//	cout << "2x7=14" << endl;
+//	cout << "2x8=16" << endl;
+//	cout << "2x9=18" << endl;
+//	cout << "2x10=20" << endl;
+//	system("pause");
+//	return 0;
+//}
+//
+// 
+// 
+// 
+// 
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "                                **************" << endl;
+//	cout << "                                 Result Sheet" << endl;
+//	cout << "                                **************" << endl;
+//	cout << "Student's rol no.             Math          Physics     Chemistry          Total" << endl;
+//	cout << "-----------------------------------------------------------------------------------" << endl;
+//	cout << "      2357                     99             85           97               281" << endl;
+//	cout << "-----------------------------------------------------------------------------------" << endl;
+//	system("pause");
+//	return 0;
+//}
+// 
+// 
+// 
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "25/3" << endl;
+//	cout << "20-12/4*2" << endl;
+//	cout << "32%7" << endl;
+//	cout << "3-7%7" << endl;
+//	cout << "18-0/4" << endl;
+//	cout << "28-5/2.0" << endl;
+//	cout << "17+5%2-3" << endl;
+//	cout << "15.0+3.0*2.0/5.0" << endl;
+//	system("pause");
+//	return 0;
+//}
+//
+// 
+// 
+// 
+// 
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "Variable Value : 13"
+//	cout << "Press any key to continue"
+//		system("pouse")
+//		return 0;
+//}
+////
+//
+//
+///* Name : Muhammad Kaleem Ullah
+//   Roll Number: L1F24BSCS0934
+//*/
+//
+//
+//
+//
+//
+//
+//
+//															LAB :2
+//
+//
+//
+//
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "************" <<
+//		endl;
+//	cout << "Hello World!" <<
+//		endl;
+//	cout << "************" <<
+//		endl;
+//}
+//
+//
+//
+//
+///*	Name: Muhammad Kaleem Ullah
+//	Roll No: L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "************\n";
+//	cout << "*          *\n";
+//	cout << "************\n";
+//}
+//
+//
+//
+//
+///*	Name: Muhammad Kaleem Ullah
+//	Roll No: L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "   *\n";
+//	cout << "  * *\n";
+//	cout << " *   *\n";
+//	cout << "*******\n";
+//}
+//
+//
+//
+//#include <iostream>
+//
+//int main()
+//{
+//	std::cout << "Hello World!\n";
+//}
+//
+//
+//
+//
+///*	Name: Muhammad Kaleem Ullah
+//	Roll No: L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "  ****\n";
+//	cout << " *    *\n";
+//	cout << " *    *\n";
+//	cout << "  ****\n";
+//}
+//
+//
+//
+///*	Name: Muhammad Kaleem Ullah
+//	Roll No: L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "*******\n";
+//	cout << "*     *\n";
+//	cout << "*     *\n";
+//	cout << "*******\n";
+//}
+//
+//
+//
+///*	Name: Muhammad Kaleem Ullah
+//	Roll No: L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "  *\n";
+//	cout << " * *\n";
+//	cout << "*   *\n";
+//	cout << " * *\n";
+//	cout << "  *\n";
+//}
+//
+//
+//
+//
+///* Name : Muhammad Kaleem Ullah
+//   Roll Number : L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "    *\n";
+//	cout << "  ****\n";
+//	cout << "********\n";
+//	cout << "  ****\n";
+//	cout << "    *\n";
+//}
+//
+//
+//
+///* Name : Muhammad Kaleem Ullah
+//   Roll Number : L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "@@@@@@@@@@@@@@\n";
+//	cout << "@  @      @  \n";
+//	cout << "@ @        @ @\n";
+//	cout << "@@          @@\n";
+//	cout << "@            @\n";
+//	cout << "@@@@@@@@@@@@@@\n";
+//}
+//
+//
+//
+//
+///* Name : Muhammad Kaleem Ullah
+//   Roll Number : L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//
+//
+//	cout << "*********\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "*********\n";
+//	cout << "*********\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "    *\n";
+//	cout << "*********\n";
+//	cout << "*\n";
+//	cout << "*\n";
+//	cout << "*\n";
+//	cout << "*\n";
+//	cout << "*\n";
+//	cout << "*\n";
+//	cout << "*********\n";
+//}
+//
+//
+//
+//
+//
+//
+//
+///*
+//Name : Muhammad Kaleem Ullah
+//Roll Number : L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "Name:Muhammad Kaleem Ullah\n";
+//	cout << "University ID: L1F24BSCS0934\n";
+//	cout << "Father Name:Mubarik Ali\n";
+//	cout << "Phone Number:03416564955\n";
+//	cout << "Address:564 Dubai Town Raiwind Road Lahore\n";
+//	cout << "Semester:1\n";
+//	cout << "Course:Introduction To Computing Lab\n";
+//}
+//
+//
+//
+//
+//
+///*
+//Name : Muhammad Kaleem Ullah
+//Roll Number : L1F24BSCS0934
+//*/
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "*********************************\n";
+//	cout << "*   programming Assignment      *\n";
+//	cout << "*    Computer programing 1      *\n";
+//	cout << "* Author:Muhammad Kaleem Ullah  *\n";
+//	cout << "*  Due Date: thrusday, jan,24   *\n";
+//	cout << "*********************************\n";
+//}
+//
+//
+//
+//																		LAB : 1
+//
+//
+//
+//
+//#include <iostream>
+//
+//int main()
+//{
+//	std::cout << "Hello World!\n";
+//}
