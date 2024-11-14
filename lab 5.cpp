495e3807d0ff9e8c2b357d30405c970fdfc28271
K786-spe <raimuhammadkaleem@gmail.com>
2024-11-14 23:20:58 +0500

    lab 5 of ITC

---
+++ lab 5.cpp
@@ -0,0 +1,65 @@
+/*include<iostream>
+using namespace std;
+int main()
+{
+	int variable = 13;
+	cout << " value of variable: " << variable << endl;
+	return 0;
+}
+ 
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int variable;
+	cout << " Enter the variable: " << endl;
+	cin >> variable;
+	cout << "value of variable:" << variable << endl;
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int vari1, vari2, sum, difference, product, division;
+	cout << " value of vari1:" << endl;
+	cin >> vari1;
+	cout << "value of vari2" << endl;
+	cin >> vari2;
+	sum = vari1 + vari2;
+	difference = vari1 - vari2;
+	product = vari1 * vari2;
+	division = vari1 / vari2;
+	cout << "variable sum:" << sum << endl;
+	cout << "variable difference:" << difference << endl;
+	cout << "variable product:" << product << endl;
+	cout << "variable division:" << division << endl;
+
+
+	return 0;
+}*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int fuel, rent, bills;
+	cout << " Enter value of fuel: " << endl;
+	cin >> fuel;
+	cout << "Enter value of rent: " << endl;
+	cin >> rent;s
+	cout << "Enter value of bills: " << endl;
+	cin >> bills;
+	cout << "---------------------------------------" << endl;
+	cout << " fuel: " << fuel << endl;
+	cout << " rent: " << rent << endl;
+	cout << " bills: " << bills << endl;
+	system(paused);
+
+	return 0;
+}
