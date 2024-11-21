07efd76a0fbcbf7b0a7d3b43e7f90bfe87ba0764
K786-spe <raimuhammadkaleem@gmail.com>
2024-11-21 10:38:46 +0500

    sav to compleat later

---
+++ Source.cpp
@@ -0,0 +1,114 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int price1, price2, price3, quality1, quality2, quality3, value1, value2, value3;
+//	cout << "Ener price of wheat:";
+//	cin >> price1;
+//	cout << "Ener price of rice:";
+//	cin >> price2;
+//	cout << "Ener price of sugar:";
+//	cin >> price3;
+//	cout << "Ener quality of wheat:";
+//	cin >> quality1;
+//	cout << "Ener quality of rice:";
+//	cin >> quality2;
+//	cout << "Ener quality of sugar:";
+//	cin >> quality3;
+//	value1 = price1*quality1;
+//	value2 = price2*quality2;
+//	value3 = price3*quality3;
+//	cout << "value of wheat:" << value1 << endl;
+//	cout << "value of rice:" << value2 << endl;
+//	cout << "value of sugar:" << value3 << endl;
+//	return 0;
+//
+//}
+//
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int month;
+//	cout << "entre month :";
+//	cin >> month;
+//	switch (month)
+//	{
+//	case 1:
+//		cout << "January" << endl;
+//		break;
+//	case 2:
+//		cout << "February" << endl;
+//		break;
+//	case 3:
+//		cout << "March" << endl;
+//		break;
+//	case 4:
+//		cout << "April" << endl;
+//		break;
+//	case 5:
+//		cout << "May" << endl;
+//		break;
+//	case 6:
+//		cout << "June" << endl;
+//		break;
+//	case 7:
+//		cout << "July" << endl;
+//		break;
+//	case 8:
+//		cout << "August" << endl;
+//		break;
+//	case 9:
+//		cout << "september" << endl;
+//		break;
+//	case 10:
+//		cout << "October" << endl;
+//		break;
+//	case 11:
+//		cout << "November" << endl;
+//		break;
+//	case 12:
+//		cout << "December" << endl;
+//		break;
+//	default :
+//		cout << "invalid number" << endl;
+//	}
+//}
+
+/*
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1,num2;
+	char operation;
+	cout << "entre 1st integr :";
+	cin >> num1;
+	cout << "entre 2nd integr :";
+	cin >> num2;
+	cout << "entre opration :" << endl;
+	cin >> operation;
+	switch (operation)
+	{
+	case '+':
+		cout << "result:" <<num1 + num2 << endl;
+		break;
+	case '-':
+		cout << "result:" << num1-num2 << endl;
+		break;
+	case '*':
+		cout << "resuly" << num1*num2 << endl;
+		break;
+	case '/':
+		cout << "result" << num1/num2 << endl;
+		break;
+	default:
+		cout << "Invalid Oppration" << endl;
+	}
+}
+*/
+#include <iostream>
+using namespace std;
+
+int main()
